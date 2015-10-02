e823fbd613a28a1bff5f3266dbfef01d6e1bbaa8
Rachel Nabors <rachelnabors@gmail.com>
2015-10-01 21:28:51 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "http://rubygems.org"
+
+gem 'sass'
+gem 'compass'
+gem 'oily_png'
---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function(){
+
+    notify = function(note) {
+      console.log(note);
+    }
+    /*
+    var waypoint = new Waypoint({
+      element: document.getElementById('mesopelagic'),
+      handler: function() {
+        console.log('Basic waypoint triggered')
+      }
+    })*/
+
+    var segments = document.getElementsByClassName('segment');
+
+    Array.prototype.forEach.call(segments, function(child) {
+      new Waypoint.Inview({
+        element: child,
+        enter: function(direction) {
+          this.element.classList.add("in-view");
+          notify(child + ': Enter triggered with direction ' + direction)
+        },
+        entered: function(direction) {
+          notify(child + ': Entered triggered with direction ' + direction)
+        },
+        exit: function(direction) {
+          notify(child + ': Exit triggered with direction ' + direction)
+        },
+        exited: function(direction) {
+          notify(child + ': Exited triggered with direction ' + direction)
+          this.element.classList.remove("in-view");
+        }
+      })
+    });
+})();
