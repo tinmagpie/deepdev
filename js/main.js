9f91d91113c7efd4d1abaa24b4d5cf21f1184984
Rachel Nabors <rachelnabors@gmail.com>
2015-10-02 18:05:42 -0700

    Adding ocean motion.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 (function(){
     "use strict";
 
-    notify = function(note) {
-      console.log(note);
-    }
     /*
     var waypoint = new Waypoint({
       element: document.getElementById('mesopelagic'),
@@ -19,16 +16,12 @@
         element: child,
         enter: function(direction) {
           this.element.classList.add("in-view");
-          notify(child + ': Enter triggered with direction ' + direction)
         },
-        entered: function(direction) {
-          notify(child + ': Entered triggered with direction ' + direction)
-        },
-        exit: function(direction) {
-          notify(child + ': Exit triggered with direction ' + direction)
-        },
+        // entered: function(direction) {
+        // },
+        // exit: function(direction) {
+        // },
         exited: function(direction) {
-          notify(child + ': Exited triggered with direction ' + direction)
           this.element.classList.remove("in-view");
         }
       })
