70e5e9bfa9bca31edf7967af85e1bfc76af10dab
Rachel Nabors <rachelnabors@gmail.com>
2015-10-06 14:42:10 -0700

    Adding in-view events to zones. Good for music. Take note, Potch.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,47 @@
 (function(){
-    "use strict";
+  "use strict";
 
-    /*
-    var waypoint = new Waypoint({
-      element: document.getElementById('mesopelagic'),
-      handler: function() {
-        console.log('Basic waypoint triggered')
+  /*
+  var waypoint = new Waypoint({
+    element: document.getElementById('mesopelagic'),
+    handler: function() {
+      console.log('Basic waypoint triggered')
+    }
+  })*/
+
+  var zones = document.getElementsByClassName('zone');
+
+  Array.prototype.forEach.call(zones, function(child) {
+    new Waypoint.Inview({
+      element: child,
+      enter: function(direction) {
+        this.element.classList.add("in-zone");
+      },
+      // entered: function(direction) {
+      // },
+      // exit: function(direction) {
+      // },
+      exited: function(direction) {
+        this.element.classList.remove("in-zone");
       }
-    })*/
+    })
+  });
 
-    var segments = document.getElementsByClassName('segment');
+  var segments = document.getElementsByClassName('segment');
 
-    Array.prototype.forEach.call(segments, function(child) {
-      new Waypoint.Inview({
-        element: child,
-        enter: function(direction) {
-          this.element.classList.add("in-view");
-        },
-        // entered: function(direction) {
-        // },
-        // exit: function(direction) {
-        // },
-        exited: function(direction) {
-          this.element.classList.remove("in-view");
-        }
-      })
-    });
+  Array.prototype.forEach.call(segments, function(child) {
+    new Waypoint.Inview({
+      element: child,
+      enter: function(direction) {
+        this.element.classList.add("in-view");
+      },
+      // entered: function(direction) {
+      // },
+      // exit: function(direction) {
+      // },
+      exited: function(direction) {
+        this.element.classList.remove("in-view");
+      }
+    })
+  });
 })();
